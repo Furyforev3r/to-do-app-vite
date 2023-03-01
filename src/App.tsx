7cb2f91389979ca22596c2edaf7032175162bb33
Messias <messiasfuryforever@gmail.com>
2023-03-01 18:56:15 -0300

    To do

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+function App() {
+  const [items, setItems] = useState([{text: 'Call a function', id: 1}, {text: 'Call a function', id: 2}, {text: 'Call a function', id: 3}])
+  const [inputtext, setInputtext] = useState('')
+  function RemoveFromList(id: number) {
+    setItems(items.filter((i) => i.id !== id))
+  }
+  
+  return (
+    <>
+    <header>
+      <h1>To do App</h1>
+    </header>
+    <main>
+      <form className='submit'>
+        <label>
+          <input type='text' className='textin' placeholder='Buy a milk...' onChange={(e) => {setInputtext(e.target.value)}} onSubmit={(e) => {e.preventDefault()}}/>
+          <input type='submit' value={'Add task...'} onClick={(e) => {e.preventDefault(), setItems([...items, {text: inputtext, id: items.length+1}])}}/>
+        </label>
+      </form>
+      <div>
+        {items.map((item) => {
+          return (
+            <div className='item'>
+              <form>
+                <label className='checkboxlabel'>
+                  <input type='checkbox'/>
+                  <span className='checkmark'></span>
+                </label>
+              </form>
+              <p><i>{item.id}. </i>{item.text}</p>
+              <FontAwesomeIcon icon={faTrash} className='trash' onClick={() => RemoveFromList(item.id)}/>
+            </div>
+          )
+        })}
+      </div>
+    </main>
+    <footer>
+      <h2>Github Projects</h2>
+    </footer>
+    </>
+  )
+}
+
+export default App
